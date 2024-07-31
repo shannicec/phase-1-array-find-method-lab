b415ea89ef49fc4d5c9a8a95ee6e8bcefbe3f294
shannicec <shanice.cheptoo@student.moringaschool.com>
2024-07-31 12:35:18 +0300

    array find lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(records) {
+    const win = records.find(record => record.result === "W");
+    
+    return win ? win.year : undefined;
+  }
+  
+  const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ];
+  
+  console.log(superbowlWin(record)); 
